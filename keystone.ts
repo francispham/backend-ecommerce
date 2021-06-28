23c50272f9ead4291cb21eafecf24253fcc21255
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-06-28 16:48:35 -0700

    Setup TS Backend with Keystone

---
+++ keystone.ts
@@ -0,0 +1,34 @@
+// https://next.keystonejs.com/docs/apis/config#system-configuration-api
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 365, // How long should they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+// https://next.keystonejs.com/docs/apis/config
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: Add data seeding here:
+  },
+  // https://next.keystonejs.com/docs/apis/schema
+  lists: createSchema({
+    //  TODO: Add Schema items here:
+  }),
+  ui: {
+    //  TODO: Change this for Roles:
+    isAccessAllowed: () => true,
+  },
+  //  TODO: Add Session Values here:
+})
