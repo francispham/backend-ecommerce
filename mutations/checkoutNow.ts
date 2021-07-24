06474dde8ea9847323f144c5dbb6a2104ec30590
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-07-24 10:32:47 -0700

    Checkout Feature III: Implemented Checkout Authentication, Calculation Logics and Creating Payment via Stripe API

--- mutations/checkoutNow.ts
+++ mutations/checkoutNow.ts
@@ -1,21 +1,82 @@
 import { KeystoneContext } from '@keystone-next/types';
 
-import { OrderCreateInput } from '../.keystone/schema-types';
+import stripeConfig from '../lib/stripe';
+import { Session } from '../types';
+import {
+  OrderCreateInput,
+  CartItemCreateInput,
+} from '../.keystone/schema-types';
 
 interface Arguments {
   token: string;
 }
+
+const graphql = String.raw;
 
 export default async function checkoutNow(
   root: any,
   { token }: Arguments,
   context: KeystoneContext
 ): Promise<OrderCreateInput> {
+  // ? Check if the User is logged in
+  const { session } = context as { session: Session };
+  const userId = session.itemId;
+  if (!userId) {
+    throw new Error('User not logged in');
+  }
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: graphql`
+      id
+      name
+      email
+      cart {
+        id
+        quantity
+        product {
+          id
+          name
+          price
+          description
+          photo {
+            id
+            image {
+              id
+              publicUrlTransformed
+            }
+          }
+        }
+      }      
+    `,
+  });
+  console.dir(user, { depth: null }); // ? `depth: null` ensures Unlimited Recursion
+
+  // ? Calculate the total price for the User's Order
+  const cartItems = user.cart.filter(
+    (cartItem: CartItemCreateInput) => cartItem.product
+  ); // ? Filter out existing products
+  const amount = cartItems.reduce(
+    (tally: number, cartItem: CartItemCreateInput) =>
+      tally + cartItem.product.price * cartItem.quantity,
+    0
+  );
+  console.log('amount:', amount);
+
+  // ? Create the Payment with the Stripe API
+  const charge = await stripeConfig.paymentIntents
+    .create({
+      amount,
+      currency: 'usd',
+      confirm: true,
+      payment_method: token,
+    })
+    .catch((error) => {
+      console.error('Error creating Payment Intent:', error);
+      throw Error(error.message);
+    });
+  console.log('Charge:', charge);
   /* 
   TODO Steps:
-  1. Check if the User is logged in
-  2. Calculate the total price for the User's Order
-  3. Create the Payment with the Stripe API
   4. Convert the CartItems to OrderItems
   5. Create the Order and return it
   */
